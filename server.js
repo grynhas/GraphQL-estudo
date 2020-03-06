0d51017f389da2cca8e732ba2e8cc450f9428263
grynhas <patrick.d.s.reis@gmail.com>
2020-03-06 15:10:07 -0300

    atualização de repositorio

---
+++ server.js
@@ -0,0 +1,34 @@
+const { GraphQLServer } = require('graphql-yoga')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/database/tabelas')
+const Operacoes = require('./infraestrutura/operations')
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  }
+
+  console.log('conectou no banco')
+
+  Tabelas.init(conexao)
+})
+const Clientes = new Operacoes('cliente')
+
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando!",
+    clientes: () => Clientes.lista(),
+    cliente: (root, {id})=> Clientes.buscaPorId(id)
+  },
+  Mutation: {
+    adicionarCliente:(root, params) => Clientes.adiciona(params),
+    atualizarCliente:(root, params) => Clientes.atualiza(params),
+    deletarCLiente:(root, {id}) => Clientes.deleta(id) 
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+})
+servidor.start(() => console.log('servidor ouvindo'))
