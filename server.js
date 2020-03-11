83cc996cddca8b1f391d6ca509bb0dc466b94c9f
grynhas <patrick.d.s.reis@gmail.com>
2020-03-11 12:38:04 -0300

    ultimas alteraçoes

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const resolvers = {
     status: () => "Servidor rodando!",
     clientes: () => Clientes.lista(),
     cliente: (root, {id})=> Clientes.buscaPorId(id),
-    pets: () => Pets.lista()
+    pets: () => Pets.lista(),
+    pet: (root, {id}) => Pets.buscaPorId(id)
+
   },
   Mutation: {
     adicionarCliente:(root, params) => Clientes.adiciona(params),
